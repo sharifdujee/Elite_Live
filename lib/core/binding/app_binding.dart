2075f03967c1a8043ec4f275200841a154624b02
sharifdujee <sharifdujee>
2025-10-14 10:24:30 +0600

    basic folder structure complete

---
+++ lib/core/binding/app_binding.dart
@@ -0,0 +1,38 @@
+import 'package:get/get.dart';
+
+class AppBinding extends Bindings {
+
+  @override
+  void dependencies() {
+   /* Get.put(SplashController());
+    Get.put(OnboardingController());
+    Get.put(NavController());
+    Get.put(HomeController());
+    Get.put(SplashController());
+    Get.put(OnboardingController());
+    Get.put(AuthController());
+    Get.put(VerificationCodeController());
+    Get.put(ThemeController());
+
+    ///Get.put(EducationController());
+    /// added by sharif
+    Get.lazyPut<FrequentlyAskingQuestionController>(()=>FrequentlyAskingQuestionController(),fenix: true);
+    Get.lazyPut<WithdrawalRequestController>(()=>WithdrawalRequestController(), fenix: true);
+    Get.lazyPut<EarningController>(()=>EarningController(), fenix: true);
+    Get.lazyPut<MessagingController>(()=>MessagingController(), fenix: true);
+    Get.lazyPut<EducationController>(()=>EducationController(), fenix: true);
+    Get.lazyPut<ThemeController>(()=>ThemeController(), fenix: true);
+    Get.lazyPut<AimingRoleController>(()=>AimingRoleController(), fenix: true);
+    Get.lazyPut<ProfessionalExperienceController>(()=>ProfessionalExperienceController(), fenix: true);
+    Get.lazyPut<AnnualSalaryController>(()=>AnnualSalaryController(), fenix: true);
+    Get.lazyPut<StartJobController>(()=>StartJobController(), fenix: true);
+    Get.lazyPut<InterviewSkillsController>(()=>InterviewSkillsController(), fenix: true);
+    Get.lazyPut<ProfileEditController>(()=>ProfileEditController(), fenix: true);
+    Get.lazyPut<CreateServiceController>(()=>CreateServiceController(), fenix: true);*/
+
+
+
+
+
+  }
+}
---
+++ lib/features/live/presentation/screen/live_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LiveScreen extends StatelessWidget {
+  const LiveScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Live"),
+    );
+  }
+}
